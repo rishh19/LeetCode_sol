d3d209d7efc29acc1769c1447f99de7bdcadde5e
Rishav Kumar Shrivastava <235rishavstar@gmail.com>
2026-08-14 11:31:29 +0530

    Time: 4 ms (53.93%) | Memory: 20.1 MB (43.98%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -1,24 +1,25 @@
 class Solution:
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+    def intervalIntersection(self, flist: List[List[int]], slist: List[List[int]]) -> List[List[int]]:
 
         result = []
 
-        i = 0
-        j = 0
+        i = 0 # points to current interval of flist
+        j = 0 # points to current interval of slist
+        
 
-        while i < len(firstList) and j < len(secondList):
+        while i < len(flist) and j < len(slist):
 
-            start1 = firstList[i][0]
-            end1 = firstList[i][1]
+            start1 = flist[i][0]
+            end1 = flist[i][1]
 
-            start2 = secondList[j][0]
-            end2 = secondList[j][1]
+            start2 = slist[j][0]
+            end2 = slist[j][1]
 
             # Find overlap
             start = max(start1, start2)
             end = min(end1, end2)
 
-            # If they overlap
+            #they overlap, append them
             if start <= end:
                 result.append([start, end])
 
